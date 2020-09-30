889046b5342105d9009e18e274381f7e979b4519
satyam-lnmi <72143318+satyam-lnmi@users.noreply.github.com>
2020-09-30 18:57:19 +0530

    Update india_corona_update.py
    check

--- india_corona_update.py
+++ india_corona_update.py
@@ -20,10 +20,6 @@ item_list=mydata_str.split("\n\n")
 data_list=[]
 for item in item_list[37:41]:
     data_list.append(item.split("\n"))
-# print(data_list[0][2])
-# print(data_list[1][0])
-# print(data_list[2][1])
-# print(data_list[3][1])
 result="Active Cases :"+str(data_list[0][2])+ "\nCured :"+str(data_list[1][0])+"\nDeaths :"+str(data_list[2][1])+"\nTotal Confirmed cases :"+str(data_list[3][1])
 n.show_toast("corona virus update",result, duration = 10,icon_path="img.ico" )
 
